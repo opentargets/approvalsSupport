7005c88da1ac0474a20a19bee2f585a0b15f2f58
polrus <polina@ebi.ac.uk>
2023-04-24 12:41:00 +0100

    OR plot v3

--- src/OR_plot.R
+++ src/OR_plot.R
@@ -6,24 +6,37 @@ library("tidyverse")
 # install.packages("epitools")
 library(epitools)
 
-data_in <- read_csv("./output/2013-2022_approvals_v2_hasGE.csv")
+data_in <- read_csv("./output/2013-2022_approvals_v3.csv")
 data  <- subset(data_in, targetIds != "NA" & has_GE != "NA")
 
 
-# Create boolean columns with Review status
+# Create boolean columns with Review status and modalities
 data$Priority <- grepl("P", data$Review_type)
 data$Breakthrough <- grepl("B", data$Review_type)
-data$Accelerated <- grepl("A", data$Review_type)
+# data$Accelerated <- grepl("A", data$Review_type)
 data$Orphan <- grepl("O", data$Review_type)
+data$SmallMol <- grepl("Small molecule", data$drugType)
+data$Ab <- grepl("Antibody", data$drugType)
+data$Protein <- grepl("Protein|Enzyme", data$drugType)
+data$Oligonucl <- grepl("Oligonucleotide", data$drugType)
 
-# write.table(data, sep = ",", file = "./output/2013-2022_approvals_v2_review.csv", row.names = FALSE)
+# write.table(data, sep = ",", file = "./output/2013-2022_approvals_v3_mod.csv", row.names = FALSE)
 
 
 # create a 2x2 contingency table with "has_GE" values as rows and the review status values as columns.
 table_data_P <- table(data$has_GE, data$Priority)
 table_data_B <- table(data$has_GE, data$Breakthrough)
-table_data_A <- table(data$has_GE, data$Accelerated)
+# table_data_A <- table(data$has_GE, data$Accelerated) # Odds ration is not applicable
 table_data_O <- table(data$has_GE, data$Orphan)
+table_data_SmallMol <- table(data$has_GE, data$SmallMol)
+table_data_Ab <- table(data$has_GE, data$Ab)
+table_data_Protein <- table(data$has_GE, data$Protein)
+table_data_Oligonucl <- table(data$has_GE, data$Oligonucl)
+
+# table_data_SmallMol
+# table_data_Ab
+# table_data_Protein
+# table_data_Oligonucl
 
 colnames(table_data_P) <- c("Other", "Priority")
 rownames(table_data_P) <- c("no_GE", "has_GE")
@@ -31,11 +44,23 @@ rownames(table_data_P) <- c("no_GE", "has_GE")
 colnames(table_data_B) <- c("Other", "Breakthrough")
 rownames(table_data_B) <- c("no_GE", "has_GE")
 
-colnames(table_data_A) <- c("Other", "Accelerated")
-rownames(table_data_A) <- c("no_GE", "has_GE")
+# colnames(table_data_A) <- c("Other", "Accelerated")
+# rownames(table_data_A) <- c("no_GE", "has_GE")
 
 colnames(table_data_O) <- c("Other", "Orphan")
 rownames(table_data_O) <- c("no_GE", "has_GE")
+
+colnames(table_data_SmallMol) <- c("Other", "SmallMol")
+rownames(table_data_SmallMol) <- c("no_GE", "has_GE")
+
+colnames(table_data_Ab) <- c("Other", "Ab")
+rownames(table_data_Ab) <- c("no_GE", "has_GE")
+
+colnames(table_data_Protein) <- c("Other", "Protein")
+rownames(table_data_Protein) <- c("no_GE", "has_GE")
+
+colnames(table_data_Oligonucl) <- c("Other", "Oligonucl")
+rownames(table_data_Oligonucl) <- c("no_GE", "has_GE")
 
 
 # dimnames(table_data_P) <- list(has_GE = c(TRUE, FALSE), Priority = c(TRUE, FALSE))
@@ -45,11 +70,11 @@ rownames(table_data_O) <- c("no_GE", "has_GE")
 
 # fisher_result <- fisher.test(table_data)
 
-# # Create a vector of correction factors
-correction_factor <- ifelse(table_data_A < 5, 1, 0)
+# # # Create a vector of correction factors
+# correction_factor <- ifelse(table_data_A < 5, 1, 0)
 
-# Apply Yates' correction to the contingency table_data_A
-table_corrected_A <- table_data_A + correction_factor
+# # Apply Yates' correction to the contingency table_data_A
+# table_corrected_A <- table_data_A + correction_factor
 
 
 odds_ratio_df <- function(table_data) {
@@ -84,41 +109,55 @@ odds_ratio_df <- function(table_data) {
 odds_ratio_1 <- odds_ratio_df(table_data_P)
 odds_ratio_2 <- odds_ratio_df(table_data_B)
 odds_ratio_3 <- odds_ratio_df(table_data_O)
-odds_ratio_4 <- odds_ratio_df(table_corrected_A)
+# odds_ratio_4 <- odds_ratio_df(table_corrected_A)
+odds_ratio_5 <- odds_ratio_df(table_data_SmallMol)
+odds_ratio_6 <- odds_ratio_df(table_data_Ab)
+odds_ratio_7 <- odds_ratio_df(table_data_Protein)
+odds_ratio_8 <- odds_ratio_df(table_data_Oligonucl)
+
 
 # Combine results into one data frame
-all_odds_ratios <- rbind(odds_ratio_1, odds_ratio_2, odds_ratio_3, odds_ratio_4)
+all_odds_ratios <- rbind(odds_ratio_1, odds_ratio_2, odds_ratio_3, odds_ratio_5, odds_ratio_6, odds_ratio_7, odds_ratio_8)
 
-# # View results
-# all_odds_ratios
+# View results
+all_odds_ratios
+
+# write.table(all_odds_ratios, sep = ",", file = "./output/2013-2022_approvals_v3_OR.csv", row.names = FALSE)
 
 
-all_odds_ratios <- all_odds_ratios[order(all_odds_ratios$estimate), ]
 
-# Create a new column with the y-axis position of each group
-all_odds_ratios$y <- seq(nrow(all_odds_ratios))
 
-# Plot the forest plot
-ggplot(all_odds_ratios, aes(x = estimate, y = table_data)) +
-  geom_point(size = 4, color = "blue") +
-  geom_errorbarh(aes(xmin = lower, xmax = upper), height = 0, color = "blue") +
-  xlim(c(0, 16)) +
-  scale_y_reverse(breaks = seq(nrow(df)), labels = all_odds_ratios$group) +
-  theme_classic() +
-  theme(axis.title.y = element_blank(),
-        axis.line.y = element_blank(),
-        axis.text.y = element_text(size = 12),
-        axis.ticks.y = element_blank(),
-        axis.title.x = element_text(size = 12),
-        axis.text.x = element_text(size = 12),
-        panel.grid.major.y = element_blank(),
-        panel.grid.minor.y = element_blank(),
-        panel.grid.major.x = element_line(color = "grey", size = 0.5),
-        panel.grid.minor.x = element_blank(),
-        panel.background = element_blank(),
-        legend.position = "none")
+
+
+
+all_odds_ratios <- read_csv("./output/2013-2022_approvals_v3_OR.csv")
+
+df <- all_odds_ratios[order(all_odds_ratios$estimate), ]
+
+
+
+# Create a factor variable for table_data to use in the y-axis
+df$table_data <- factor(df$table_data, levels = df$table_data)
+
+ggplot(df, aes(x = estimate, y = table_data)) +
+  geom_point(size = 3) +
+  geom_errorbarh(aes(xmin = lower, xmax = upper), height = 0.2) +
+  geom_vline(xintercept = 1, linetype = "dashed") +
+  scale_x_log10(limits = c(0.2, 10)) +
+  scale_y_discrete(expand = c(0.1, 0)) +
+  labs(x = "Odds Ratio (log scale)", y = "") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(colour = "black"),
+        axis.text = element_text(size = 16, face = "bold"),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size = 16, face = "bold"),
+        plot.margin = unit(c(0.5, 1, 0.5, 0.5), "cm")) +
+  geom_text(aes(label = paste0("p = ", round(fisher_p_value, 3)), x = upper + 0.3),
+            hjust = 0, size = 6)
 
 
 ggsave("./output/OR_v1.png", 
-        width = 6,
-        height = 3)
+        width = 10,
+        height = 8)
