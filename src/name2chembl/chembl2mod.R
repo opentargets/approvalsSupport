3915f3c6c3388df8f8c2580c3886b9b285d2a778
Polina Rusina <polina@ebi.ac.uk>
2023-04-24 10:15:10 +0000

    Modalities added

--- src/name2chembl/chembl2mod.R
+++ src/name2chembl/chembl2mod.R
@@ -12,45 +12,32 @@ config <- spark_config()
 config$spark.hadoop.fs.gs.requester.pays.mode <- "AUTO" # nolint
 config$spark.hadoop.fs.gs.requester.pays.project.id <- "open-targets-eu-dev" # nolint
 
-# spark connect
+
 sc <- spark_connect(master = "yarn", config = config)
 
 
-local_approvals <- read_csv("./data/2013-2022/2013-2022_approvals_GE_v2_in.csv")
+local_approvals <- read_csv("./output/2013-2022_approvals_v2_review.csv")
+approvals <- sdf_copy_to(sc, local_approvals, overwrite = TRUE)
 
 
-# Add MoAs to final table
-# Extra MoAs required to fill the gaps
-new_moas <- read_csv("./data/ammend_data/amend_moas.csv")
-new_moas <- sdf_copy_to(sc, new_moas, overwrite = TRUE)
-
+# Read Platform data (Dataset: Drug)
 gs_path <- "gs://open-targets-data-releases/"
-moa_path <- paste(
+drug_path <- paste(
     gs_path, data_release,
-    "/output/etl/parquet/mechanismOfAction/",
+    "/output/etl/parquet/molecule/",
     sep = ""
 )
 
-# available MoAs + ammended
-moa <- spark_read_parquet(sc, moa_path, memory = FALSE) %>%
-    select(chemblIds, targets) %>%
-    sdf_explode(chemblIds) %>%
-    sdf_explode(targets) %>%
-    rename(targetId = targets) %>%
-    sdf_distinct() %>%
-    sdf_bind_rows(new_moas)
-
-MoAs <- local_approvals %>%
-  left_join(moa, by = c("DrugId" = "chemblIds"), copy=TRUE) %>%
-  mutate(targetId = replace_na(targetId, "")) %>%
-  group_by(Drug_name) %>%
-  summarise(targetIds = paste(targetId, collapse = "; ")) %>%
-  rowwise() %>%
-  mutate(targetIds = toString(unique(unlist(strsplit(targetIds, "; ")))))
-
-local_approvals_MoAs <- local_approvals %>%
-  left_join(MoAs, by = "Drug_name") 
+# ChEMBL ID from Platform
+drug_info <- spark_read_parquet(sc, drug_path) %>%
+    select(id, drugType) %>%
+    sdf_distinct()
 
 
-write.table(local_approvals_MoAs , sep = ",", file = "./data/2013-2022/2013-2022_approvals_GE_v2_out.csv", row.names = FALSE)
+# Drug name to chembl id
+approvals_mod <- approvals %>%
+    left_join(drug_info, by = c("DrugId" = "id")) %>% 
+    collect()
 
+approvals_mod %>% 
+    write.csv("./output/2013-2022_approvals_v2_review_mod.csv")
