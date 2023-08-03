9946d761caf12da456b5629699a578679ed5c037
Polina Rusina <polina@ebi.ac.uk>
2023-08-03 12:39:21 +0000

    GE_types modified

--- code/GE_types_add.R
+++ code/GE_types_add.R
@@ -5,17 +5,8 @@ library(ggplot2)
 library("tidyverse")
 
 
-data <- read_csv("./results/2013-2022_approvals_GE_by_source_test.csv")
-local_approvals <- read_csv("./results/2013-2022_approvals_GE_test.csv")
-
-approvals <- local_approvals %>%
-    group_by(brandDrugName, originalDrugName, drugId) %>%
-    summarise(drugId = paste(",", drugId),
-              targetIds = paste(",", targetIds),
-              interactorIds = paste(",", interactorIds),
-              diseaseIds = paste(",", diseaseIds)
-              # relatedIds = paste(",", relatedIds)
-              )
+data <- read_csv("./results/2013-2022_approvals_GE_map.csv")
+local_approvals <- read_csv("./results/2013-2022_approvals_GE_amend.csv")
 
 # Conditions for any GE support for approval (score>0, interactor or related disease has GE)
 data_hasAnyGE <- data %>% filter(noTarget == FALSE) %>% # remove all non-human targets
@@ -40,7 +31,7 @@ approvals_hasAnyGE <- local_approvals %>%
 
 
 # GE support subtyping by evidence (original target/interactor + original disease/related disease)
-data_GE_types <- approvals_hasAnyGE %>% 
+data_GE_types <- data %>% filter(noTarget == FALSE) %>% # remove all non-human targets
     mutate(score_zero_bool = score == 0, 
            interactionAssociation_bool = replace_na(interactionAssociation, FALSE),
            phenotypeAssociation_bool = replace_na(phenotypeAssociation, FALSE)
@@ -50,12 +41,12 @@ data_GE_types <- approvals_hasAnyGE %>%
       hasInteractorDiseasesGE = any(interactionAssociation_bool & score_zero_bool) & !any(!score_zero_bool),
       hasTargetRelatedGE = any(phenotypeAssociation_bool & score_zero_bool) & !any(!score_zero_bool),
       hasInteractorRelatvedGE = any(phenotypeAssociation_bool & interactionAssociation_bool & score_zero_bool) & !any(!score_zero_bool),
-      hasTargetDiseaseGE = any(hasAnyGE) & !any(hasInteractorDiseasesGE) & !any(hasTargetRelatedGE) & !any(hasInteractorRelatvedGE)
+      hasTargetDiseaseGE = any(!score_zero_bool) & !any(hasInteractorDiseasesGE) & !any(hasTargetRelatedGE) & !any(hasInteractorRelatvedGE)
     ) %>%
     ungroup()
 
+approvals_GE_types <- approvals_hasAnyGE %>%
+  left_join(data_GE_types, by = "originalDrugName", "yearApproval")
 
 
-
-
-write.table(approvals_GE, sep = ",", file = "./results/2013-2022_approvals_GE.csv", row.names = FALSE)
+write.table(approvals_GE_types, sep = ",", file = "./results/2013-2022_approvals_GE_final.csv", row.names = FALSE)
